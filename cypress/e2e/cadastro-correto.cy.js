36e1f796f0647b98b667fa3a9bb8a2c9394405ab
Jitterkkk <jitterzibetti@gmail.com>
2026-02-27 13:51:53 -0300

    Ajuste no metodo cy.get

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -2,7 +2,7 @@ describe('Página de Cadastro', () => {
   it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
     // cypress contains é usado para encontrar um elemento que contenha um texto específico
-    cy.contains('a','Cadastrar').click();
+    cy.get('[data-test="register-button"]').click();
     // cypress get é usado para encontrar um elemento com um seletor específico, nesse caso, um input com o nome "nome"
     cy.get('[data-test="input-name"]').type('Biazus');
     cy.get('[data-test="input-email"]').type('biazus@email.com');
