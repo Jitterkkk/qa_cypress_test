da817590c07bc1de9a6675ec1569efc80804df0a
Jitterkkk <jitterzibetti@gmail.com>
2026-03-02 14:36:56 -0300

    @stubPost dublê para testes

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,15 +1,29 @@
 describe("Página de Login", () => {
-  before(() => {
-    cy.visit("https://adopet-frontend-cypress.vercel.app/");
+  beforeEach(() => {
+    cy.visit("https://adopet-frontend-cypress.vercel.app");
     cy.get('[data-test="login-button"]').click();
+    cy.intercept(
+      "POST",
+      "https://adopet-api-i8qu.onrender.com/adotante/login",
+      {
+        statusCode: 400,
+      },
+    ).as("stubPost");
   });
 
-  it("Deve preencher os campos do formulário incorretamente e exibir mensagens de erro ao usuário na página", () => {
-    cy.get('[data-test="input-loginEmail"]').type("biazus");
-    cy.get('[data-test="input-loginPassword"]').type("senha");
+  it("Verifica mensagem de falha no login", () => {
     cy.get('[data-test="submit-button"]').click();
+    cy.contains("É necessário informar um endereço de email").should(
+      "be.visible",
+    );
+    cy.contains("Insira sua senha").should("be.visible");
+  });
 
-    cy.contains("Por favor, verifique o email digitado").should("be.visible");
-    cy.contains("A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres").should("be.visible");
+  it("Deve exibir uma mensagem de erro ao tentar fazer login com credenciais incorretas", () => {
+    cy.login("ana@email.com", "Senha123");
+    cy.wait("@stubPost");
+    cy.contains("Falha no login. Consulte suas credenciais.").should(
+      "be.visible",
+    );
   });
 });
